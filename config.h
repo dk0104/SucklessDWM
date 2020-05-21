8c9eed98d785a0d339f6e4a23cec531075e7b4a3
Denis Keksel <Denis.Keksel@getslash.de>
2020-05-21 21:12:21 +0200

    backup

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[] = { "Wuncon Siji", "Mononoki Nerd Font:size=9:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Source Code Pro:size=13";
+static const char *fonts[] = { "SF Pro Display:size=13:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Source Code Pro:size=12";
 static const char col_night1[]      = "#2E3440";
 static const char col_night2[]      = "#3B4252";
 static const char col_night4[]      = "#4C566a";
@@ -33,7 +33,7 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "120x34", NULL };
+const char *spcmd1[] = {"st", "-n", "spterm", "-g", "144x41", NULL };
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
 const char *spcmd3[] = {"rocketchat-desktop", NULL };
 static Sp scratchpads[] = {
@@ -86,7 +86,7 @@ static const Layout layouts[] = {
 /* commands */
 static char dmenumon[2] = "0"; /* component of dmenucmd, manipulated in spawn() */
 static const char *dmenucmd[] = { "dmenu_run_history", "-m", dmenumon, "-fn", dmenufont, "-nb", col_night1, "-nf", col_snow1, "-sb", col_frost3, "-sf", col_night1, NULL };
-static const char *termcmd[]  = { "termite", NULL };
+static const char *termcmd[]  = { "kitty", NULL };
 static const char *lockcmd[] = {"betterlockscreen","-l",NULL};
 static const char *switchMonSettings[] = {"dmenu_monitor",NULL};
 static const char *shutdowm[] = {"dmenu_shutdown",NULL};
