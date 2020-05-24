0a592c276e942661e64208eecbdbf70f6f2e135a
Denis Keksel <Denis.Keksel@getslash.de>
2020-05-24 22:15:20 +0200

    font changed

--- config.h
+++ config.h
@@ -10,8 +10,8 @@ static const int systraypinningfailfirst = 1;   /* 1: if pinning fails, display 
 static const int showsystray        = 1;     /* 0 means no systray */
 static const int showbar            = 1;        /* 0 means no bar */
 static const int topbar             = 1;        /* 0 means bottom bar */
-static const char *fonts[] = { "SF Pro Display:size=13:antialias=true:autohint=true" };
-static const char dmenufont[]       = "Source Code Pro:size=12";
+static const char *fonts[] = { "SF Pro Display:size=11:antialias=true:autohint=true" };
+static const char dmenufont[]       = "Source Code Pro:size=11";
 static const char col_night1[]      = "#2E3440";
 static const char col_night2[]      = "#3B4252";
 static const char col_night4[]      = "#4C566a";
@@ -33,7 +33,9 @@ typedef struct {
 	const char *name;
 	const void *cmd;
 } Sp;
-const char *spcmd1[] = {"st", "-n", "spterm", "-g", "144x41", NULL };
+//const char *spcmd1[] = {"st", "-n", "spterm", "-g", "144x41", NULL };
+const char *spcmd1[] = {"kitty", "--name", "spterm", "-session", "~/.config/kitty/sessions/sp", NULL };
+
 const char *spcmd2[] = {"st", "-n", "spfm", "-g", "144x41", "-e", "ranger", NULL };
 const char *spcmd3[] = {"rocketchat-desktop", NULL };
 static Sp scratchpads[] = {
@@ -44,6 +46,7 @@ static Sp scratchpads[] = {
 };
 /* tagging */
 static const char *tags[] = { "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+//static const char *tags[] = { "●", "●", "●", "●", "●", "●", "●", "●" };
 static const Rule rules[] = {
 	/* xprop(1):
 	 *	WM_CLASS(STRING) = instance, class
